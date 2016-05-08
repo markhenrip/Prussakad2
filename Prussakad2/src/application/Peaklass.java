f9d56a3a09ccc25245d56f09f26e698ad06e054a
markhenrip <markhenripedoson@gmail.com>
2016-05-08 17:00:29 +0300

    Alustus

---
+++ Prussakad2/src/application/Peaklass.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Peaklass extends Application{
+
+	@Override
+	public void start(Stage põhi) throws Exception {
+		//TODO Write
+		
+	}
+
+}
